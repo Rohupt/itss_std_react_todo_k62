57e9655e1aa5a4a5066f13f33680f7d732403585
EC2 Default User <ec2-user@ip-172-31-20-55.ec2.internal>
2021-04-08 02:49:23 +0000

    create by other component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -32,12 +32,9 @@ function Todo() {
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
-      {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
-      ))}
+      {items.map(item =>
+        <TodoItem item={item}/>
+      )}
       <div className="panel-block">
         {items.length} items
       </div>
